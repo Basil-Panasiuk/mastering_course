5081c835c176fd2ada02aceedeaf2cf18ad0077e
Basil-Panasiuk <v.panasiuk.job@gmail.com>
2023-06-15 13:31:47 +0300

    Add lesson dynamic route

---
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -0,0 +1,7 @@
+<template>
+  <h2>Lesson</h2>
+  <p>This is a lesson</p>
+  <p>Chapter slug: {{ $route.params.chapterSlug }}</p>
+  <p>Chapter slug: {{ $route.params.lessonSlug }}</p>
+</template>
+<script setup lang="ts"></script>
