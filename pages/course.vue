e46fd30b5faf58aee798cf808c410a97c5dfac5d
basil-p-anasiuk <panasiuk.vasyl@tftc.dev>
2023-09-16 21:03:50 +0300

    Feat: add client/server sides error handling

--- pages/course.vue
+++ pages/course.vue
@@ -36,7 +36,16 @@
     </div>
 
     <div class="prose p-12 bg-white rounded-md w-[65ch]">
-      <NuxtPage />
+      <NuxtErrorBoundary>
+        <NuxtPage />
+        <template #error="{ error }">
+          <p>
+            Oh, no, something went wrong with the lesson!
+            <code>{{ error }}</code>
+          </p>
+          <p><button @click="resetError(error)">Reset</button></p>
+        </template>
+      </NuxtErrorBoundary>
     </div>
   </div>
 </template>
@@ -44,4 +53,11 @@
 import { useCourse } from "~/composables/useCourse";
 
 const { chapters } = useCourse();
+
+const resetError = async (error) => {
+  await navigateTo(
+    "/course/chapter/1-chapter-1/lesson/1-introduction-to-typescript-with-vue-js-3"
+  );
+  error.value = null;
+};
 </script>
